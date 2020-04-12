cd369a6492e1dcf9da1a8355b10d30b7cce8c093
Edson Mesquita <emesquita@Edsons-MacBook-Pro.local>
2020-04-12 18:59:35 -0400

    core set

--- src/views/Heroes.vue
+++ src/views/Heroes.vue
@@ -10,106 +10,13 @@
 
 <script>
 import Hero from "../components/hero/Hero.vue";
+import coreset from "../cards/coreset";
 
 export default {
   name: "HelloWorld",
   components: { Hero },
   data: () => ({
-    heroes: [
-      {
-        name: "Wolverine",
-        team: 4,
-        cards: [
-          { 
-            name: "Keen Senses" ,
-            hc: 2,
-            rarity: 1,
-            cost: 2,
-            attack: "1",
-            abilities: [
-              [ { hc: 2 }, ": Draw a card." ]
-            ]
-          },
-          { 
-            name: "Healing Factor",
-            hc: 2,
-            rarity: 1,
-            cost: 3,
-            attack: "2",
-            abilities: [
-              "You may KO a Wound from your hand or discard pile. If you do, draw a card."
-            ]
-          }, 
-          { 
-            name: "Frenzied Slashing",
-            hc: 2,
-            rarity: 2,
-            cost: 5,
-            attack: "2",
-            abilities: [
-              [ { hc: 2 }, ": Draw two cards." ]
-            ]
-          }, 
-          { 
-            name: "Berserker Rage",
-            hc: 2,
-            rarity: 3,
-            cost: 8,
-            attack: "0+",
-            "abilities": [
-              "Draw three cards.",
-              [
-                {
-                  "hc": 2
-                },
-                ": You get",
-                {
-                  "bold": " +1"
-                },
-                {
-                  "icon": 1 
-                },
-                "for each extra card you've drawn this turn."
-              ]
-            ]
-          }]
-      },
-      {
-        name: "Thor",
-        team: 1,
-        cards: [
-          { 
-            name: "Odinson" ,
-            hc: 4,
-            rarity: 1,
-            cost: 3,
-            recruit: "2+"
-          }, 
-          { 
-            name: "Surge of Power",
-            hc: 3,
-            rarity: 1,
-            cost: 4,
-            recruit: "2",
-            attack: "0+"
-          }, 
-          { 
-            name: "Call Lightning",
-            hc: 3,
-            rarity: 2,
-            cost: 6,
-            attack: "3+"
-          }, 
-          { 
-            name: "God of Thunder",
-            hc: 3,
-            rarity: 3,
-            cost: 8,
-            recruit: "5",
-            attack: "0+"
-          }]
-      }
-    ]
+    heroes: coreset.heroes
   })
 };
 </script>
