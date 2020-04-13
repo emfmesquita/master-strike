5391753b638c761c6e54cc4f61438d2c75b3a009
Edson Mesquita <emesquita@Edsons-MacBook-Pro.local>
2020-04-12 20:27:37 -0400

    style

--- src/components/hero/Hero.vue
+++ src/components/hero/Hero.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">{{ hero.name }}</v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="4" md="3"  v-for="card in hero.cards" :key="card.name">
-        <HeroCard :card="card" :hero="hero"/>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-card class="mx-auto hero" outlined>
+    <v-container>
+      <v-row>
+        <v-col cols="12 title">{{ hero.name }}</v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="4" md="3"  v-for="card in hero.cards" :key="card.name">
+          <HeroCard :card="card" :hero="hero"/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
@@ -21,3 +23,9 @@ export default {
   data: () => ({})
 };
 </script>
+
+<style scoped lang="scss">
+.hero {
+  background-color:#f2f2f2;
+}
+</style>
