ee10b3a7193732699954016ee5bf311fba5861c8
Edson Mesquita <emesquita@Edsons-MacBook-Pro.local>
2020-04-12 21:25:36 -0400

    Team icon

--- src/components/hero/CardAbility.vue
+++ src/components/hero/CardAbility.vue
@@ -3,12 +3,14 @@
     <span v-if="typeof ability === 'string'">{{ ability }}</span>
     <span class="font-weight-bold" v-else-if="ability.bold">{{ ability.bold }}</span>
     <HeroClassIcon class="icon" v-else-if="ability.hc" :icon="ability.hc"/>
+    <TeamIcon class="icon" v-else-if="ability.team" :icon="ability.team"/>
     <AbilityIcon v-else-if="ability.icon" :icon="ability.icon" width="24px"/>
   </span>
 </template>
 
 <script>
 import HeroClassIcon from "./HeroClassIcon.vue";
+import TeamIcon from "./TeamIcon.vue";
 import AbilityIcon from "./AbilityIcon.vue";
 
 export default {
@@ -16,6 +18,7 @@ export default {
   props: ["ability"],
   components: {
     HeroClassIcon,
+    TeamIcon,
     AbilityIcon
   },
   data: () => ({})
