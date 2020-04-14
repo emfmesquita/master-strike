6dd697550e36f1ddac26c70e2ec08faf4459ba38
anaccerq <anaccerq@gmail.com>
2020-04-14 16:10:47 -0400

    Keywords + Ranged Icon

--- src/cards/revelations.js
+++ src/cards/revelations.js
@@ -44,7 +44,7 @@ export default {
               },
               ": ",
               {
-                keyword: 1
+                keyword: 66
               }
             ]
           ]
--- src/constants/keywords.js
+++ src/constants/keywords.js
@@ -6,13 +6,433 @@ const BRIBE = {
     value: "bribe",
     label: "Bribe"
 }
+const VERSATILE = {
+    value: "Versatile",
+    label: "Versatile"
+}
+const FOCUS = {
+    value: "focus",
+    label: "Focus"
+}
+const BURROW = {
+    value: "burrow",
+    label: "Burrow"
+}
+const COSMIC_THREAT = {
+    value: "cosmicthreat",
+    label: "Cosmic Threat"
+}
+const GALACTUS_COSMIC_THREAT = {
+    value: "galactuscosmicthreat",
+    label: "Galactus Cosmic Threat"
+}
+const WALL_CRAWL = {
+    value: "wallcrawl",
+    label: "Wall-Crawl"
+}
+const FEAST = {
+    value: "feast",
+    label: "Feast"
+}
+const DEMOLISH = {
+    value: "demolish",
+    label: "Demolish"
+}
+const DODGE = {
+    value: "dodge",
+    label: "Dodge"
+}
+const ELUSIVE = {
+    value: "elusive",
+    label: "Elusive"
+}
+const X_TREME_ATTACK = {
+    value: "xtremeattack",
+    label: "X-Treme Attack"
+}
+const ARTIFACT = {
+    value: "artifact",
+    label: "Artifact"
+}
+const THROWN_ARTIFACT = {
+    value: "thrownartifact",
+    label: "Thrown Artifact"
+}
+const URU_ENCHANTED_WEAPONS = {
+    value: "uruenchantedweapons",
+    label: "Uru-Enchanted Weapons"
+}
+const RISE_OF_THE_LIVING_DEAD = {
+    value: "riseofthelivingdead",
+    label: "Rise of The Living Dead"
+}
+const CROSS_DIMENSIONAL_RAMPAGE = {
+    value: "crossdimensionalrampage",
+    label: "Cross-Dimensional Rampage"
+}
+const SPECTRUM = {
+    value: "spectrum",
+    label: "Spectrum"
+}
+const PATROL = {
+    value: "patrol",
+    label: "Patrol"
+}
+const CIRCLE_OF_KUNG_FU = {
+    value: "circleofkungfu",
+    label: "Circle of Kung-Fu"
+}
+const CIRCLE_OF_QUACK_FU = {
+    value: "circleofquackfu",
+    label: "Circle of Quack-Fu"
+}
+const FATEFUL_RESURRECTION = {
+    value: "fatefulresurrection",
+    label: "Fateful Resurrection"
+}
+const CHARGE = {
+    value: "charge",
+    label: "Charge"
+}
+const MAN_OUT_OF_TIME = {
+    value: "manoutoftime",
+    label: "Man Out of Time"
+}
+const WOMAN_OUT_OF_TIME = {
+    value: "womanoutoftime",
+    label: "Woman Out of Time"
+}
+const SAVIOR = {
+    value: "savior",
+    label: "Savior"
+}
+const ABOMINATION = {
+    value: "abomination",
+    label: "Abomination"
+}
+const DIVIDED = {
+    value: "divided",
+    label: "Divided"
+}
+const SIZE_CHANGING = {
+    value: "sizechanging",
+    label: "Size Changing"
+}
+const PHASING = {
+    value: "phasing",
+    label: "Phasing"
+}
+const FORTIFY = {
+    value: "Fortify",
+    label: "Fortify"
+}
+const SHIELD_CLEARANCE = {
+    value: "shieldclearance",
+    label: "S.h.i.e.l.d. Clearance"
+}
+const EXCESSIVE_VIOLENCE = {
+    value: "excessiveviolence",
+    label: "Excessive Violence"
+}
+const REVENGE = {
+    value: "revenge",
+    label: "Revenge"
+}
+const INVESTIGATE = {
+    value: "investigate",
+    label: "Investigate"
+}
+const HIDDEN_WITNESS = {
+    value: "hiddenwitness",
+    label: "Hidden Witness"
+}
+const X_GENE = {
+    value: "xgene",
+    label: "X-gene"
+}
+const PIERCING_ENERGY = {
+    value: "piercingenergy",
+    label: "Piercing Energy"
+}
+const BERSERK = {
+    value: "berserk",
+    label: "Berserk"
+}
+const SOARING_FLIGHT = {
+    value: "soaringflight",
+    label: "Soaring Flight"
+}
+const LIGHTSHOW= {
+    value: "lightshow",
+    label: "Lightshow"
+}
+const DOMINATE= {
+    value: "dominate",
+    label: "Dominate"
+}
+const HUMAN_SHIELDS= {
+    value: "humanshields",
+    label: "Human Shields"
+}
+const TRAPS= {
+    value: "traps",
+    label: "Traps"
+}
+const TOKEN_CARDS= {
+    value: "tokencards",
+    label: "Token Cards"
+}
+const DANGER_SENSE= {
+    value: "dangersense",
+    label: "Danger Sense"
+}
+const STRIKER= {
+    value: "striker",
+    label: "Striker"
+}
+const COORDINATE= {
+    value: "coordinate",
+    label: "Coordinate"
+}
+const CHEERING_CROWDS= {
+    value: "cheeringcrowds",
+    label: "Coordinate"
+}
+const TRANSFORM = {
+    value: "transform",
+    label: "Transform"
+}
+const OUTWIT= {
+    value: "outwit",
+    label: "Outwit"
+}
+const SMASH = {
+    value: "smash",
+    label: "Smash"
+}
+const WOUNDED_FURY = {
+    value: "woundedfury",
+    label: "Wounded Fury"
+}
+const CONQUEROR = {
+    value: "conqueror",
+    label: "Conqueror"
+}
+const MICROSCOPIC_SIZE_CHANGING = {
+    value: "microscopicsizechanging",
+    label: "Microscopic Size-Changing"
+}
+const EMPOWERED = {
+    value: "empowered",
+    label: "Empowered"
+}
+const CHIVALROUS_DUEL = {
+    value: "chivalrousduel",
+    label: "Chivalrous Duel"
+}
+const SYMBIOTE_BONDS = {
+    value: "symbiontebonds",
+    label: "Symbionte Bonds"
+}
+const POISON_VILLAINS = {
+    value: "poisonvillains",
+    label: "Poison Villains"
+}
+const DIGEST = {
+    value: "digest",
+    label: "Digest"
+}
+const INDIGESTION = {
+    value: "indigestion",
+    label: "Indigestion"
+}
+const SWITCHEROO = {
+    value: "switcheroo",
+    label: "Switcheroo"
+}
+const HYPERSPEED = {
+    value: "hyperspeed",
+    label: "Hyperspeed"
+}
+const DARK_MEMORIES = {
+    value: "darkmemories",
+    label: "Dark Memories"
+}
+const LAST_STAND = {
+    value: "laststand",
+    label: "Last Stand"
+}
+const LOCATIONS = {
+    value: "Locations",
+    label: "Locations"
+}
+const TRANSFORMING_SCHEMES = {
+    value: "transformingschemes",
+    label: "Transforming Schemes"
+}
+const UNDERCOVER = {
+    value: "undercover",
+    label: "Undercover"
+}
+const SHIELD_LEVEL = {
+    value: "shieldlevel",
+    label: "Shield Level"
+}
+const HYDRA_LEVEL = {
+    value: "hydralevel",
+    label: "Hydra Level"
+}
+const ADAPTING_MASTERMINDS= {
+    value: "adaptingmasterminds",
+    label: "Adapting Masterminds"
+}
 
 export const keywords = Object.freeze({
     TELEPORT,
-    BRIBE
+    BRIBE,
+    VERSATILE,
+    FOCUS,
+    BURROW,
+    COSMIC_THREAT,
+    GALACTUS_COSMIC_THREAT,
+    WALL_CRAWL,
+    FEAST,
+    DEMOLISH,
+    DODGE,
+    ELUSIVE,
+    X_TREME_ATTACK,
+    ARTIFACT,
+    THROWN_ARTIFACT,
+    URU_ENCHANTED_WEAPONS,
+    RISE_OF_THE_LIVING_DEAD,
+    CROSS_DIMENSIONAL_RAMPAGE,
+    SPECTRUM,
+    PATROL,
+    CIRCLE_OF_KUNG_FU,
+    CIRCLE_OF_QUACK_FU,
+    FATEFUL_RESURRECTION,
+    CHARGE,
+    MAN_OUT_OF_TIME,
+    WOMAN_OUT_OF_TIME,
+    SAVIOR,
+    ABOMINATION,
+    DIVIDED,
+    SIZE_CHANGING,
+    PHASING,
+    FORTIFY,
+    SHIELD_CLEARANCE,
+    EXCESSIVE_VIOLENCE,
+    REVENGE,
+    INVESTIGATE,
+    HIDDEN_WITNESS,
+    X_GENE,
+    PIERCING_ENERGY,
+    BERSERK,
+    SOARING_FLIGHT,
+    LIGHTSHOW,
+    DOMINATE,
+    HUMAN_SHIELDS,
+    TRAPS,
+    TOKEN_CARDS,
+    DANGER_SENSE,
+    STRIKER,
+    COORDINATE,
+    CHEERING_CROWDS,
+    TRANSFORM,
+    OUTWIT,
+    SMASH,
+    WOUNDED_FURY,
+    CONQUEROR,
+    MICROSCOPIC_SIZE_CHANGING,
+    EMPOWERED,
+    CHIVALROUS_DUEL,
+    SYMBIOTE_BONDS,
+    POISON_VILLAINS,
+    DIGEST,
+    INDIGESTION,
+    SWITCHEROO,
+    HYPERSPEED,
+    DARK_MEMORIES,
+    LAST_STAND,
+    LOCATIONS,
+    TRANSFORMING_SCHEMES,
+    UNDERCOVER,
+    SHIELD_LEVEL,
+    HYDRA_LEVEL,
+    ADAPTING_MASTERMINDS
 });
 
 export const keywordsArray = Object.freeze([
-    TELEPORT,   // 1
-    BRIBE       // 2
+    TELEPORT,                   // 1
+    BRIBE,                      // 2
+    VERSATILE,                  // 3
+    FOCUS,                      // 4
+    BURROW,                     // 5
+    COSMIC_THREAT,              // 6
+    GALACTUS_COSMIC_THREAT,     // 7
+    WALL_CRAWL,                 // 8
+    FEAST,                      // 9
+    DEMOLISH,                   // 10
+    DODGE,                      // 11
+    ELUSIVE,                    // 12
+    X_TREME_ATTACK,             // 13
+    ARTIFACT,                   // 14
+    THROWN_ARTIFACT,            // 15
+    URU_ENCHANTED_WEAPONS,      // 16
+    RISE_OF_THE_LIVING_DEAD,    // 17
+    CROSS_DIMENSIONAL_RAMPAGE,  // 18
+    SPECTRUM,                   // 19
+    PATROL,                     // 20
+    CIRCLE_OF_KUNG_FU,          // 21
+    CIRCLE_OF_QUACK_FU,         // 22
+    FATEFUL_RESURRECTION,       // 23
+    CHARGE,                     // 24
+    MAN_OUT_OF_TIME,            // 25
+    WOMAN_OUT_OF_TIME,          // 26
+    SAVIOR,                     // 27
+    ABOMINATION,                // 28
+    DIVIDED,                    // 29
+    SIZE_CHANGING,              // 30
+    PHASING,                    // 31
+    FORTIFY,                    // 32
+    SHIELD_CLEARANCE,           // 33
+    EXCESSIVE_VIOLENCE,         // 34
+    REVENGE,                    // 35
+    INVESTIGATE,                // 36
+    HIDDEN_WITNESS,             // 37
+    X_GENE,                     // 38
+    PIERCING_ENERGY,            // 39
+    BERSERK,                    // 40
+    SOARING_FLIGHT,             // 41
+    LIGHTSHOW,                  // 42
+    DOMINATE,                   // 43
+    HUMAN_SHIELDS,              // 44
+    TRAPS,                      // 45
+    TOKEN_CARDS,                // 46
+    DANGER_SENSE,               // 47
+    STRIKER,                    // 48
+    COORDINATE,                 // 49    
+    CHEERING_CROWDS,            // 50
+    TRANSFORM,                  // 51
+    OUTWIT,                     // 52
+    SMASH,                      // 53
+    WOUNDED_FURY,               // 54
+    CONQUEROR,                  // 55
+    MICROSCOPIC_SIZE_CHANGING,  // 56
+    EMPOWERED,                  // 57
+    CHIVALROUS_DUEL,            // 58
+    SYMBIOTE_BONDS,             // 59
+    POISON_VILLAINS,            // 60
+    DIGEST,                     // 61
+    INDIGESTION,                // 62
+    SWITCHEROO,                 // 63
+    HYPERSPEED,                 // 64
+    DARK_MEMORIES,              // 65
+    LAST_STAND,                 // 66
+    LOCATIONS,                  // 67
+    TRANSFORMING_SCHEMES,       // 68
+    UNDERCOVER,                 // 69
+    SHIELD_LEVEL,               // 70
+    HYDRA_LEVEL,                // 71
+    ADAPTING_MASTERMINDS        // 72
 ]);
