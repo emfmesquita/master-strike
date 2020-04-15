6943db26333ff4913e728c87e26468464cce366e
anaccerq <anaccerq@gmail.com>
2020-04-15 15:27:27 -0400

    All teams

--- src/constants/team.js
+++ src/constants/team.js
@@ -14,17 +14,120 @@ const X_MEN = {
     value: "x-men",
     label: "X-Men"
 };
+const CHAMPIONS = {
+    value: "champions",
+    label: "Champions"
+};
+const WARBOUND = {
+    value: "warbound",
+    label: "Warbound"
+};
+const CRIME_SYNDICATE = {
+    value: "crime-Syndicate",
+    label: "Crime Syndicate"
+};
+const FANTASTIC_FOUR = {
+    value: "fantastic-four",
+    label: "Fantastic Four"
+};
+const MARVEL_KNIGHTS = {
+    value: "marvel-knights",
+    label: "Marvel Knights"
+};
+const X_FORCE = {
+    value: "x-force",
+    label: "X-Force"
+};
+const SINISTER_SIX = {
+    value: "sinister-six",
+    label: "Sinister Six"
+};
+const FOES_OF_ASGARD = {
+    value: "foes-of-asgard",
+    label: "Foes of Asgard"
+};
+const BROTHERHOOD = {
+    value: "brotherhood",
+    label: "Brotherhood"
+};
+const GUARDIANS_OF_THE_GALAXY = {
+    value: "guardians-of-the-galaxy",
+    label: "Guardians of the Galaxy"
+};
+const ILLUMINATI = {
+    value: "illuminati",
+    label: "Illuminati"
+};
+const CABAL = {
+    value: "cabal",
+    label: "Cabal"
+};
+const HYDRA = {
+    value: "hydra",
+    label: "HYDRA"
+};
+const NEW_WARRIORS = {
+    value: "new-warriors",
+    label: "New Warriors"
+};
+const MERCS_FOR_MONEY = {
+    value: "mercs-for-money",
+    label: "Mercs for Money"
+};
+const VENOMVERSE= {
+    value: "venomverse",
+    label: "Venomverse"
+};
+const HEROES_OF_ASGARD= {
+    value: "heroes-of-asgard",
+    label: "Heroes of Asgard"
+};
 
 export const team = Object.freeze({
     AVENGERS,
     SHIELD,
     SPIDER_FRIENDS,
-    X_MEN
+    X_MEN,
+    CRIME_SYNDICATE,
+    FANTASTIC_FOUR,
+    MARVEL_KNIGHTS,
+    X_FORCE,
+    SINISTER_SIX,
+    FOES_OF_ASGARD,
+    BROTHERHOOD,
+    GUARDIANS_OF_THE_GALAXY,
+    HYDRA,
+    CABAL,
+    ILLUMINATI,
+    NEW_WARRIORS,
+    MERCS_FOR_MONEY,
+    CHAMPIONS,
+    WARBOUND,
+    VENOMVERSE,
+    HEROES_OF_ASGARD
+    
 });
 
 export const teamArray = Object.freeze([
-    AVENGERS,       // 1
-    SHIELD,         // 2
-    SPIDER_FRIENDS, // 3
-    X_MEN           // 4
+    AVENGERS,                       //1
+    SHIELD,                         //2
+    SPIDER_FRIENDS,                 //3
+    X_MEN,                          //4
+    CRIME_SYNDICATE,                //5
+    FANTASTIC_FOUR,                 //6
+    MARVEL_KNIGHTS,                 //7
+    X_FORCE,                        //8
+    SINISTER_SIX,                   //9
+    FOES_OF_ASGARD,                 //10
+    BROTHERHOOD,                    //11
+    GUARDIANS_OF_THE_GALAXY,        //12
+    HYDRA,                          //13
+    CABAL,                          //14
+    ILLUMINATI,                     //15
+    NEW_WARRIORS,                   //16
+    MERCS_FOR_MONEY,                //17
+    CHAMPIONS,                      //18
+    WARBOUND,                       //19
+    VENOMVERSE,                     //20   
+    HEROES_OF_ASGARD                //21
 ]);
