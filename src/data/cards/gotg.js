c277a84b9579374432c4f1d8f21f700c0a2a0345
anaccerq <anaccerq@gmail.com>
2020-05-01 16:57:49 -0400

    Guardians of the Galaxy Cards

--- src/data/cards/gotg.js
+++ src/data/cards/gotg.js
@@ -19,6 +19,289 @@ export default {
                 "."
               ]
             ]
+          },
+          {
+            name: "Interstellar Tracker",
+            hc: 2,
+            rarity: 1,
+            cost: 3,
+            recruit: "2",
+            abilities: [
+              "Look at the top card of your deck. Discard it or put it back.",
+              [
+                { hc: 2},
+                ": You may KO the card you discarded this way."
+              ]
+            ]
+          },
+          {
+            name: "The Destroyer",
+            hc: 2,
+            rarity: 2,
+            cost: 6,
+            attack: "4",
+            abilities: [
+              [
+                { team: 12},
+                ": Each other player reveals an ",
+                { hc: 2 },
+                " Hero or discards an Artifact they control. For each Artifact discarded this way, you gain a Shard.",
+              ]
+            ]
+          },
+          {
+            name: "Avatar of Destruction",
+            hc: 2,
+            rarity: 3,
+            cost: 7,
+            abilities: [
+              [
+                "Double the ",
+                { icon: 1 },
+                " you have."
+              ]
+            ]
+          }
+        ]
+      },
+      {
+        name: "Gamora",
+        set: 7,
+        team: 12,
+        cards: [
+          {
+            name: "Bounty Hunter",
+            hc: 1,
+            rarity: 1,
+            cost: 2,
+            recruit: "2",
+            abilities: [
+              "A Villain gains a Shard."
+            ]
+          },
+          {
+            name: "Deadliest Woman in the Universe",
+            hc: 2,
+            rarity: 1,
+            cost: 3,
+            abilities: [
+              "Gain two Shards.",
+              [
+                { hc: 1},
+                ": Gain another Shard."
+              ]
+            ]
+          },
+          {
+            name: "Galactic Assassin",
+            hc: 1,
+            rarity: 2,
+            cost: 5,
+            attack: "3",
+            abilities: [
+              [
+                "A Villain of your choice gets no ",
+                { icon: 1},
+                " from Shards this turn."
+              ],
+              [
+                { hc: 1},
+                { hc: 1},
+                ": The Mastermind gets no ",
+                { icon: 1},
+                " from Shards this turn.",
+              ]
+            ]
+          },
+          {
+            name: "Godslayer Blade",
+            hc: 1,
+            rarity: 3,
+            cost: 8,
+            abilities: [
+              { keyword: 14, text: "Artifact -" },
+              [
+                "Once per turn, gain two Shards. Once per turn, you may spend 5 Shards to get ",
+                { bold: "+10"},
+                { icon: 1 },
+                "."
+              ]
+            ]
+          }
+        ]
+      },
+      {
+        name: "Groot",
+        set: 7,
+        team: 12,
+        cards: [
+          {
+            name: "Prune the Growths",
+            hc: 4,
+            rarity: 1,
+            cost: 4,
+            attack: "2+",
+            abilities: [
+              [
+                { hc: 4},
+                ": You may KO a card from your hand or discard pile. If you do, gain a Shard."
+              ]
+            ]
+          },
+          {
+            name: "Surviving Sprig",
+            hc: 4,
+            rarity: 1,
+            cost: 3,
+            attack: "1",
+            abilities: [
+              "When you draw a new hand of cards at the end of this turn, draw an extra card."
+            ]
+          },
+          {
+            name: "Groot and Branches",
+            hc: 1,
+            rarity: 2,
+            cost: 4,
+            abilities: [
+              [
+                "Gain two Shards. You may spend Shards to get ",
+                { icon: 2},
+                " this turn."
+              ],
+              [
+                { hc: 1},
+                ": You may choose another player. That player gains a Shard."
+              ]
+            ]
+          },
+          {
+            name: "I Am Groot",
+            hc: 4,
+            rarity: 3,
+            cost: 8,
+            recruit: "5",
+            abilities: [
+              "When you recruit your next Hero this turn, you gain Shards equal to that Hero's cost."
+            ]
+          }
+        ]
+      },
+      {
+        name: "Rocket Raccoon",
+        set: 7,
+        team: 12,
+        cards: [
+          {
+            name: "Gritty Scavenger",
+            hc: 5,
+            rarity: 1,
+            cost: 3,
+            recruit: "2",
+            abilities: [
+              [
+                "You may discard a card. If you do, draw a card."
+              ]
+            ]
+          },
+          {
+            name: "Trigger Happy",
+            hc: 3,
+            rarity: 1,
+            cost: 4,
+            attack: "2",
+            abilities: [
+              [
+                { team: 12},
+                ": You gain a Shard for each other ",
+                { team: 12},
+                " Hero you played this turn."
+              ]
+            ]
+          },
+          {
+            name: "Incoming Detector",
+            hc: 2,
+            rarity: 2,
+            cost: 4,
+            abilities: [
+              { keyword: 14, text: "Artifact -" },
+              [
+                "Whenever a Master Strike or Villain's Ambush ability is completed, you may gain a Shard."
+              ]
+            ]
+          },
+          {
+            name: "Vengeance is Rocket",
+            hc: 5,
+            rarity: 3,
+            cost: 7,
+            attack: "5+",
+            abilities: [
+              [
+                { hc: 5},
+                ": You get ",
+                { bold: "+1"},
+                { icon: 1},
+                " for each Master Strike in the KO pile and/or stacked next to the Mastermind."
+              ]
+            ]
+          }
+        ]
+      },
+      {
+        name: "Star-Lord",
+        set: 7,
+        team: 12,
+        cards: [
+          {
+            name: "Element Guns",
+            hc: 3,
+            rarity: 1,
+            cost: 4,
+            recruit: "2",
+            abilities: [
+              { keyword: 14, text: "Artifact -" },
+              [
+                "Once per turn, gain a Shard."
+              ]
+            ]
+          },
+          {
+            name: "Legendary Outlaw",
+            hc: 1,
+            rarity: 1,
+            cost: 4,
+            recruit: "2",
+            abilities: [
+              [
+                "Choose an Artifact any player controls with a 'once per turn' ability. Play a copy of one of those abilities."
+              ]
+            ]
+          },
+          {
+            name: "Implanted Memory Chip",
+            hc: 5,
+            rarity: 2,
+            cost: 6,
+            abilities: [
+              { keyword: 14, text: "Artifact -" },
+              [
+                "Once per turn, draw a card."
+              ]
+            ]
+          },
+          {
+            name: "Sentient Starship",
+            hc: 3,
+            rarity: 3,
+            cost: 8,
+            abilities: [
+              { keyword: 14, text: "Artifact -" },
+              [
+                "Once per turn, gain a Shard for each Artifact you control."
+              ]
+            ]
           }
         ]
       }
