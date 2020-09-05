628ae5f5c8fc088c049f1fac1a6c6a9e6edbbb5c
emfmesquita <edson.mesquita.ismart@gmail.com>
2020-09-05 16:21:29 -0400

    card name on search

--- src/services/searchUtils.js
+++ src/services/searchUtils.js
@@ -4,6 +4,7 @@ import { getAllHeroes, processHeroText } from "./cardUtils"
 
 export const buildHeroSearch = () => {
   const search = new JsSearch.Search("name");
+  search.addIndex("name");
   search.addIndex("heroName");
   search.addIndex("abilitiesText");
   const heroes = getAllHeroes();
